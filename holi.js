1f8fc462f6edf881764cf3ec7d8d32383db2a9df
Cristian Camilo Agudelo <heycristian26@gmail.com>
2025-06-17 07:41:58 -0500

    Add files via upload

---
+++ holi.js
@@ -0,0 +1,14 @@
+let edad = prompt("Ingresa tu edad: ");
+let nombre = prompt("Ingresa tu nombre: ");
+let ciudad = prompt("Ingresa tu ciudad: ");
+
+alert(
+  "Hola, " +
+    nombre +
+    ", " +
+    "tienes " +
+    edad +
+    " años y vives en " +
+    ciudad +
+    "."
+);
